d5c2e6d81f62c4b034e2a328d45e11db77ce0c43
Chong Lip Phang <fragment_shader@yahoo.com>
2024-10-27 16:48:56 +0800

    1

--- app/ClientComponent.tsx
+++ app/ClientComponent.tsx
@@ -4,16 +4,17 @@ async function getItem(id: Number) {
   console.log('getting item for id ' + id);
   const res = await fetch(
     // The `fetch` function is automatically memoized and the result is cached
-    `https://webcodingcenternextjsserveract-31tx--3000--34c588ed.local-credentialless.webcontainer.io/api?id=${id}`
+    `localhost/api?id=${id}`,{cache:'force-cache'}
   );
+  console.log(res.json());
   return res.json();
 }
 
-export function ClientComponent() {
+export async function ClientComponent() {
   console.log('Rendering <ClientComponent />');
   getItem(0); // cache MISS : This function is only executed the first time
   getItem(1); // cache MISS
   getItem(2); // cache MISS
   getItem(1); // cache HIT
-  return <p>The last call to getItem() didn't fire fetch().</p>;
+  return <p>The last call to getItem() didn&apost fire fetch().</p>;
 }
