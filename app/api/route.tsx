87040d0b407d048116329f889281e2c851ac21d5
Chong Lip Phang <fragment_shader@yahoo.com>
2024-10-28 10:39:24 +0800

    11

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,8 +1,10 @@
 import { type NextRequest } from 'next/server';
-export const dynamic = 'force-static';
+import { revalidatePath } from 'next/cache';
+//export const dynamic = 'force-static';
 let n=0;
 export function GET(request: NextRequest) {
   console.log('fetching...' + request.nextUrl.searchParams.get('id'));
   n++;
+  revalidatePath('/api');
   return Response.json({ x: n });
 }
