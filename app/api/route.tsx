5bfe1cbd4ba7680b8f446520057b1e3d780e02e4
Chong Lip Phang <fragment_shader@yahoo.com>
2024-10-28 11:32:04 +0800

    16

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -1,12 +1,13 @@
 import { type NextRequest } from 'next/server';
 import { revalidatePath } from 'next/cache';
+import { NextResponse } from 'next/server';
 //export const dynamic = 'force-static';
 let n=0;
 export function GET(request: NextRequest) {
   console.log('fetching...' + request.nextUrl.searchParams.get('id'));
   n++;
   revalidatePath('/api?id='+ request.nextUrl.searchParams.get('id'));
-  const res =  Response.json({ x: n });
+  const res =  NextResponse.json({ x: n });
   res.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins, or specify a particular origin
   return res;
 }
